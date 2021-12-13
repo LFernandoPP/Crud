e0fb9f66c7526988f312b8fab4dc82833d3542c9
LFernandoPP <luisfernandoperespires@gmail.com>
2021-12-13 16:32:03 -0300

    Adicionado metodo que mapeia pra response

--- src/main/java/com/v1/mapper/UsuarioMapper.java
+++ src/main/java/com/v1/mapper/UsuarioMapper.java
@@ -2,13 +2,23 @@ package com.v1.mapper;
 
 import com.domain.entity.UsuarioEntity;
 import com.v1.dto.request.UsuarioRequest;
+import com.v1.dto.response.UsuarioResponse;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 public class UsuarioMapper {
 
-    public static UsuarioEntity mapUsuarioRequestToModel (UsuarioRequest usuario) {
+    public static UsuarioEntity mapUsuarioRequestToEntity(UsuarioRequest usuario) {
         return UsuarioEntity
+                .builder()
+                .email(usuario.getEmail())
+                .nome(usuario.getNome())
+                .senha(usuario.getSenha())
+                .build();
+    }
+
+    public static UsuarioResponse mapUsuarioEntityToResponse(UsuarioEntity usuario) {
+        return UsuarioResponse
                 .builder()
                 .email(usuario.getEmail())
                 .nome(usuario.getNome())
