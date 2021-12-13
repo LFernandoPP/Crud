1b0be5b9d1e4b01f6e6d471be870898632d72e54
LFernandoPP <luisfernandoperespires@gmail.com>
2021-12-13 14:59:33 -0300

    service de validação adicionado

---
+++ src/main/java/com/service/ValidacaoService.java
@@ -0,0 +1,20 @@
+package com.service;
+
+import com.domain.repository.UsuarioRepository;
+import com.exception.ApiException;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class ValidacaoService {
+
+    private UsuarioRepository repository;
+
+    public void validaUsuario(String email){
+        if(repository.findByEmail(email).getId().isEmpty()){
+            throw new ApiException("Usuario nao encontrado", HttpStatus.NOT_FOUND);
+        }
+    }
+}
