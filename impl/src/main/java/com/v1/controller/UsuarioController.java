7b4e2846094d7e3f675998580a25e8ad3521ea48
LFernandoPP <87454966+LFernandoPP@users.noreply.github.com>
2021-12-14 10:43:06 -0300

    Adicionado anotações do swagger e reponseStatus

--- impl/src/main/java/com/v1/controller/UsuarioController.java
+++ impl/src/main/java/com/v1/controller/UsuarioController.java
@@ -1,11 +1,17 @@
 package com.v1.controller;
 
+import com.exception.ExceptionResponse;
 import com.facade.UsuarioFacade;
 import com.v1.dto.request.UsuarioRequest;
 import com.v1.dto.response.UsuarioResponse;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @AllArgsConstructor
@@ -15,6 +21,13 @@ public class UsuarioController {
 
     private UsuarioFacade facade;
 
+    @ApiOperation(value = "Cria um novo usuario")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Usuario criado"),
+            @ApiResponse(code = 400, message = "Requisição ruim", response = ExceptionResponse.class),
+            @ApiResponse(code = 500, message = "Erro interno do servidor", response = ExceptionResponse.class)
+    })
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/usuario")
     public UsuarioResponse cadastra(UsuarioRequest usuario) {
         return facade.cadastra(usuario);
