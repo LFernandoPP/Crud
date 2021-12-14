b7992e219eb79f1e73754dad4379b0b8fec964d8
LFernandoPP <87454966+LFernandoPP@users.noreply.github.com>
2021-12-14 11:23:28 -0300

    Anotação Getter e Setter alterada para Data

--- impl/src/main/java/com/domain/entity/UsuarioEntity.java
+++ impl/src/main/java/com/domain/entity/UsuarioEntity.java
@@ -1,17 +1,21 @@
 package com.domain.entity;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@Setter
+@Data
 @Builder
 @Document(value = "Usuario")
 public class UsuarioEntity {
 
+    @Id
     @Indexed(unique = true)
     private String email;
     private String nome;
