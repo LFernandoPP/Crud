6776254d2e634ee18233535ad3d864919d03b470
LFernandoPP <luisfernandoperespires@gmail.com>
2021-12-14 08:32:38 -0300

    Anotação alterada para indexed

--- src/main/java/com/domain/entity/UsuarioEntity.java
+++ src/main/java/com/domain/entity/UsuarioEntity.java
@@ -1,7 +1,7 @@
 package com.domain.entity;
 
 import lombok.*;
-import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @AllArgsConstructor
@@ -9,11 +9,10 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Getter
 @Setter
 @Builder
-@Document(value = "Usuarios")
+@Document(value = "Usuario")
 public class UsuarioEntity {
 
-    @Id
-    private String id;
+    @Indexed(unique = true)
     private String email;
     private String nome;
     private String senha;
