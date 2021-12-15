d9d6ffd3615abaf66d81e705eabb2a76aee81de3
LFernandoPP <87454966+LFernandoPP@users.noreply.github.com>
2021-12-15 16:21:20 -0300

    Alterado busca por email para o uso de optional

--- impl/src/main/java/com/domain/repository/UsuarioRepository.java
+++ impl/src/main/java/com/domain/repository/UsuarioRepository.java
@@ -4,7 +4,9 @@ import com.domain.entity.UsuarioEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UsuarioRepository extends MongoRepository<UsuarioEntity, String> {
-    UsuarioEntity findByEmail(String email);
+    Optional<UsuarioEntity> findByEmail(String email);
 }
