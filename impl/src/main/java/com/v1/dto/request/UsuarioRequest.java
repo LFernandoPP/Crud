8b50664dbc3b4454fd504654878d0d7baaac4cb2
LFernandoPP <87454966+LFernandoPP@users.noreply.github.com>
2021-12-15 08:21:48 -0300

    Adicionado as anotações notEmpty

--- impl/src/main/java/com/v1/dto/request/UsuarioRequest.java
+++ impl/src/main/java/com/v1/dto/request/UsuarioRequest.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 @AllArgsConstructor
@@ -14,12 +15,15 @@ import javax.validation.constraints.Size;
 @Setter
 public class UsuarioRequest {
 
+    @NotEmpty
     @Email
     private String email;
 
+    @NotEmpty
     @Size(min = 2, max = 40, message = ("Tamanho - Min 2 | Max 40"))
     private String nome;
 
+    @NotEmpty
     @Size(min = 6, max = 40, message = ("Tamanho - Min 6 | Max 40"))
     private String senha;
 }
