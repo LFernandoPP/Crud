f98453017d06eca5e4e07bb6c4902c2764616478
LFernandoPP <87454966+LFernandoPP@users.noreply.github.com>
2021-12-15 08:46:20 -0300

    Adicionado a anotação repository

--- impl/src/main/java/com/domain/repository/UsuarioRepository.java
+++ impl/src/main/java/com/domain/repository/UsuarioRepository.java
@@ -2,7 +2,9 @@ package com.domain.repository;
 
 import com.domain.entity.UsuarioEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UsuarioRepository extends MongoRepository<UsuarioEntity, String> {
     UsuarioEntity findByEmail(String email);
 }
