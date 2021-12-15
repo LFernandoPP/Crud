c71aea631a9787ba60cd95549040b3ad11122165
LFernandoPP <87454966+LFernandoPP@users.noreply.github.com>
2021-12-15 14:31:53 -0300

    Adicionado o teste de integração e removido o metodo cadastra do teste unitario

---
+++ impl/src/test/java/com/v1/controller/UsuarioControllerTest.java
@@ -0,0 +1,49 @@
+package com.v1.controller;
+
+import com.domain.repository.UsuarioRepository;
+import com.facade.UsuarioFacade;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.service.UsuarioService;
+import com.service.ValidacaoService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static com.stubs.UsuarioEntityStub.generateUsuarioEntity;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = UsuarioController.class)
+@ContextConfiguration(classes = {UsuarioController.class,
+        UsuarioFacade.class,
+        ValidacaoService.class,
+        UsuarioService.class})
+@AutoConfigureMockMvc
+public class UsuarioControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UsuarioRepository repository;
+
+    @Test
+    public void cadastra_ReturnCode_Created() throws Exception {
+        given(repository.save(any())).willReturn(generateUsuarioEntity());
+        this.mockMvc.perform(post("/crud/v1/usuario")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper()
+                                .writeValueAsString(generateUsuarioEntity())))
+                .andExpect(status().isCreated());
+    }
+}
