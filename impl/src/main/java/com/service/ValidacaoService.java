044a37e16947eb693089d82bf38b6eb45790fa07
LFernandoPP <87454966+LFernandoPP@users.noreply.github.com>
2021-12-15 16:20:01 -0300

    Adicionado o metodo validaUsuario

--- impl/src/main/java/com/service/ValidacaoService.java
+++ impl/src/main/java/com/service/ValidacaoService.java
@@ -1,7 +1,9 @@
 package com.service;
 
 import com.domain.repository.UsuarioRepository;
+import com.exception.ApiException;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @AllArgsConstructor
@@ -9,4 +11,10 @@ import org.springframework.stereotype.Service;
 public class ValidacaoService {
 
     private UsuarioRepository repository;
+
+    public void validaUsuario(String email) {
+        if (repository.findByEmail(email).isEmpty()) {
+            throw new ApiException("Usuario não encontrado", HttpStatus.NOT_FOUND);
+        }
+    }
 }
