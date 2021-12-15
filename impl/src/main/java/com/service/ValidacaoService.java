a5c84f36394e5a55d07389444ad4090ea24719e5
LFernandoPP <87454966+LFernandoPP@users.noreply.github.com>
2021-12-15 14:50:24 -0300

    Remoção do metodo que não esta sendo utilizado

--- impl/src/main/java/com/service/ValidacaoService.java
+++ impl/src/main/java/com/service/ValidacaoService.java
@@ -1,9 +1,7 @@
 package com.service;
 
 import com.domain.repository.UsuarioRepository;
-import com.exception.ApiException;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @AllArgsConstructor
@@ -11,10 +9,4 @@ import org.springframework.stereotype.Service;
 public class ValidacaoService {
 
     private UsuarioRepository repository;
-
-    public void validaUsuario(String email){
-        if(repository.findByEmail(email).getEmail().isEmpty()){
-            throw new ApiException("Usuario nao encontrado", HttpStatus.NOT_FOUND);
-        }
-    }
 }
