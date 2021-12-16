9b46480ebb135a4b9042edc12ee955b3406c52b7
LFernandoPP <87454966+LFernandoPP@users.noreply.github.com>
2021-12-16 14:27:25 -0300

    Remoção do optional

--- impl/src/main/java/com/domain/repository/UsuarioRepository.java
+++ impl/src/main/java/com/domain/repository/UsuarioRepository.java
@@ -4,9 +4,7 @@ import com.domain.entity.UsuarioEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface UsuarioRepository extends MongoRepository<UsuarioEntity, String> {
-    Optional<UsuarioEntity> findByEmail(String email);
+    UsuarioEntity findByEmail(String email);
 }
